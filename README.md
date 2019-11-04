410ab01d50380cf3445fdd56bcb1215000295095
mharnetiaux <mattskyh@gmail.com>
2019-11-04 07:58:10 -0800

    initial commit - added packages, Readme, webpack config.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# web-dev-test
-Web Dev Code Test
+## Setup
+- make sure you have Node.js installed
+- Git clone repo `https://github.com/mharnetiaux/web-dev-test.git`
+- `cd` into cloned project
+- run `npm install`
+
+## Dev 
+- run `npm start`
+- navigate to `localhost:3000`
+
+## Build 
+- run `npm run build`. This creates a `dist/` directory in root.
+
+## View demo
+- open `index.html` file in browser - located in `dist/` directory.
+or
+- run `npm start` and open `localhost:3000`.
---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import OptimizeJSAssetsPlugin from 'babel-minify-webpack-plugin';
+
+const config = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'scripts/[name][hash].bundle.js',
+    },
+    optimization: {
+        minimizer: [new OptimizeJSAssetsPlugin, new OptimizeCSSAssetsPlugin],
+    },
+    module: {
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            use: [
+                { loader: 'babel-loader' }
+            ],
+            exclude: /node_modules/
+        },
+            {
+                test: /\.(less)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name][hash].bundle.css',
+            chunkFilename: '[id][hash].css',
+            ignoreOrder: false
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/views/index.html',
+            inject: 'body',
+            filename: 'index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devServer: {
+        port: 3000
+    },
+    devtool: "source-map"
+};
+export default config;
